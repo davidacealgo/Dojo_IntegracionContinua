c16d933396c538f68fdc40532c6b050e5a73ba12
davidacealgo <davidacevedo115@gmail.com>
2017-10-21 09:38:35 -0500

    2 version

--- app/app.js
+++ app/app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var CodeBreaker = require('./main');
 var app = express();
 
-app.set('port', (process.env.PORT || 3001));
-
 app.get('/setSecret/:secret', function(req,res){
   number = req.params.secret;
   CodeBreaker.setSecret(number);
@@ -13,10 +11,6 @@ app.get('/setSecret/:secret', function(req,res){
 app.get('/guess/:number', function(req, res){
   number = req.params.number;
   res.send({result: CodeBreaker.codeBreaker(number)})
-});
-
-app.listen(app.get('port'), function (){
-  console.log('NodeJs app listening on port', app.listen)
 });
 
 module.exports=app;
